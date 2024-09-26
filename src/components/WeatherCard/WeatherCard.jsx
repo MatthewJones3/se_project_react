aec93d145311304df7836c5565efb39d382b9833
Matthew <MattyJones1615@gmail.com>
2024-09-26 14:36:36 -0400

    commit just to have a first commit on WTWR

---
+++ src/components/WeatherCard/WeatherCard.jsx
@@ -0,0 +1,13 @@
+import "./WeatherCard.css";
+import Sunny from "../../images/Sunny1.png";
+
+function WeatherCard() {
+  return (
+    <section className="weather-card">
+      <p className="weather-card__temp">75 deg F</p>
+      <img src={Sunny} alt="Sunny" className="weather-card__image" />
+    </section>
+  );
+}
+
+export default WeatherCard;
